f50f44a521497089b367a61c51221e35069c78e1
Rahul-aithal <aithalrahul34@gmail.com>
2024-07-13 23:08:29 +0530

    Router setup in app and index

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
-export default function App() {
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+import Navbar from './components/Navbar';
+
+function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+   
+<div className='bg-blue-100 w-full h-full ' >
+      <Navbar />
+      <Outlet />
+      </div>
+
+  );
 }
+
+export default App;
